d34efa87964a9be22420192efbaea1e3b2b034ae
Prem <prem.tiwari@outlook.com>
2016-12-07 08:46:43 +0530

    added bootstrap pricing table

---
+++ index..php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex">
+    <title>Responsive pricing tables with Hover Effects</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="style.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <!-- include google analytical code -->
+            <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-44054372-1', 'auto');
+              ga('send', 'pageview');
+            </script>
+        <!-- include google analytical code -->
+</head>
+<body><br><br>
+<center>    
+      <span style="color:#fff;font-size:24px;text-align:center;">Responsive Bootstrap Pricing Table</span><br>
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- First Add --><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3256637139560604" data-ad-slot="7111527177" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      </center><br><br>
+       <!-- PRICING TABLE STYLE 1-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-12 float-shadow">        
+                    <div class="price_table_container">
+                        <div class="price_table_heading">Starter</div>
+                        <div class="price_table_body">
+                            <div class="price_table_row cost warning-bg"><strong>$ 19</strong><span>/MONTH</span></div>
+                            <div class="price_table_row">1 Website</div>
+                            <div class="price_table_row">10 GB Storage</div>
+                            <div class="price_table_row">10 GB Bandwidth</div>
+                            <div class="price_table_row">10 Email Addresses</div>
+                            <div class="price_table_row">Free Backup</div>
+                            <div class="price_table_row">Full Time Support</div>                                                
+                        </div>
+                        <a href="#" class="btn btn-warning btn-lg btn-block">Sign Up</a>
+                    </div>
+                </div>
+        
+                <div class="col-md-3 col-sm-6 col-xs-12 float-shadow">
+                    <div class="recommended"><strong><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> RECOMMENDED</strong></div>                              
+                    <div class="price_table_container">
+                        <div class="price_table_heading">Basic</div>
+                        <div class="price_table_body">
+                            <div class="price_table_row cost primary-bg"><strong>$ 29</strong><span>/MONTH</span></div>
+                            <div class="price_table_row">10 Websites</div>
+                            <div class="price_table_row">100 GB Storage</div>
+                            <div class="price_table_row">100 GB Bandwidth</div>
+                            <div class="price_table_row">50 Email Addresses</div>
+                            <div class="price_table_row">Free Backup</div>
+                            <div class="price_table_row">Full Time Support</div>                                                
+                        </div>
+                        <a href="#" class="btn btn-primary btn-lg btn-block">Sign Up</a>
+                    </div>
+                </div>
+        
+                <div class="col-md-3 col-sm-6 col-xs-12 float-shadow">        
+                    <div class="price_table_container">
+                        <div class="price_table_heading">Premium</div>
+                        <div class="price_table_body">
+                            <div class="price_table_row cost success-bg"><strong>$ 39</strong><span>/MONTH</span></div>
+                            <div class="price_table_row">100 Websites</div>
+                            <div class="price_table_row">1 TB Storage</div>
+                            <div class="price_table_row">1 TB Bandwidth</div>
+                            <div class="price_table_row">100 Email Addresses</div>
+                            <div class="price_table_row">Free Backup</div>
+                            <div class="price_table_row">Full Time Support</div>                                                
+                        </div>
+                        <a href="#" class="btn btn-success btn-lg btn-block">Sign Up</a>
+                    </div>
+                </div>
+        
+                <div class="col-md-3 col-sm-6 col-xs-12 float-shadow">        
+                    <div class="price_table_container">
+                        <div class="price_table_heading">Master</div>
+                        <div class="price_table_body">
+                            <div class="price_table_row cost info-bg"><strong>$ 60</strong><span>/MONTH</span></div>
+                            <div class="price_table_row">Unlimited Websites</div>
+                            <div class="price_table_row">10 TB Storage</div>
+                            <div class="price_table_row">100 TB Bandwidth</div>
+                            <div class="price_table_row">1000 Email Addresses</div>
+                            <div class="price_table_row">Free Backup</div>
+                            <div class="price_table_row">Full Time Support</div>                                                
+                        </div>
+                        <a href="#" class="btn btn-info btn-lg btn-block">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- PRICING TABLE STYLE 1-->
+
+</body>
+</html>
